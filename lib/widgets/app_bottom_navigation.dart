bc1129c3df3344a46a22156c31479b18bb73b06e
shafeemhsn <shafeemuhsin@gmail.com>
2025-12-01 16:41:31 +0530

    add bottom navigation bar globally

---
+++ lib/widgets/app_bottom_navigation.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavigationBar extends StatelessWidget {
+  const AppBottomNavigationBar({
+    super.key,
+    required this.currentIndex,
+    this.onTap,
+  });
+
+  final int currentIndex;
+  final ValueChanged<int>? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Records'),
+      ],
+    );
+  }
+}
